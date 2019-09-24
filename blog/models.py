dc63d462cfa8b0f261cff5a628fc2cf6a05d5b99
RonitGoldental <ronit.goldental@gmail.com>
2019-09-24 20:25:17 +0300

    blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    blogger_name=models.CharField(max_length=10)
+    massage = models.CharField(max_length=200)

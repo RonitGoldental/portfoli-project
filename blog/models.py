5d2d11fc9f4c24b54fedf6feaea470f5709c73af
RonitGoldental <ronit.goldental@gmail.com>
2019-09-29 16:42:51 +0300

    blog added

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     message = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.message[:10]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
